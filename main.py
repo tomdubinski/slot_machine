2f15cf753088647b6f9daba4a6967df52d1a007c
tomd <tomdubinski@gmail.com>
2022-10-14 11:57:15 +0100

    added get_slot_machine function

--- main.py
+++ main.py
@@ -8,12 +8,31 @@ MIN_BET = 1 #dollars
 NUM_ROWS = 3
 NUM_COLS = 3
 
-symbol_count = { #define the number of symbols
+symbol_count = { #define the symbols in a dict
     "A": 2,
     "B": 4,
     "C": 6,
     "D": 8
 }
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:] #slice operator so original is preserved
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
 
 def deposit():
     while True:
@@ -53,7 +72,6 @@ def get_bet():
         else:
             print("please enter a number.")
     return amount
-
 
 def main():
     balance = deposit()
